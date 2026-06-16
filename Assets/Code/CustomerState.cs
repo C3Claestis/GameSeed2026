ec4d685da172cbac63aae38e3298653adfc849a7
MJ Sidik <darkimanity@gmail.com>
2026-06-16 18:25:10 +0700

    membuat base script untuk gamemanager serta menambahkan enum state untuk service -note: bisa mulai dari buat template scriptable nya sesuai core loop

---
+++ Assets/Code/CustomerState.cs
@@ -0,0 +1,8 @@
+public enum CustomerState
+{
+    None,
+    Waiting,
+    Appearing,
+    Active,
+    Served
+}
--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -1,18 +1,85 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float globalTimer = 200f;
+
+    [SerializeField] private GameObject pembeli;
+
+    private CustomerState currentState = CustomerState.None;
+
+    private void Start()
     {
-        
+        InvokeRepeating(nameof(ReduceTimer), 1f, 1f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ReduceTimer()
     {
-        
+        globalTimer--;
+
+        if (globalTimer > 0 && currentState == CustomerState.None)
+        {
+            StartCoroutine(SpawnPembeli());
+        }
+
+        if (globalTimer <= 0)
+        {
+            globalTimer = 0;
+            CancelInvoke(nameof(ReduceTimer));
+        }
+    }
+
+    private IEnumerator SpawnPembeli()
+    {
+        currentState = CustomerState.Waiting;
+
+        // Jeda sebelum muncul
+        yield return new WaitForSeconds(2f);
+
+        currentState = CustomerState.Appearing;
+
+        CanvasGroup canvas = pembeli.GetComponent<CanvasGroup>();
+
+        if (canvas != null)
+        {
+            yield return StartCoroutine(FadeCanvas(canvas, 0, 1, 0.5f));
+        }
+
+        currentState = CustomerState.Active;
+    }
+
+    private IEnumerator FadeCanvas(CanvasGroup canvas, float start, float end, float duration)
+    {
+        float elapsed = 0f;
+
+        canvas.alpha = start;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            canvas.alpha = Mathf.Lerp(start, end, elapsed / duration);
+            yield return null;
+        }
+
+        canvas.alpha = end;
+    }
+
+    public void ServeCustomer()
+    {
+        if (currentState != CustomerState.Active)
+            return;
+
+        currentState = CustomerState.Served;
+
+        CanvasGroup canvas = pembeli.GetComponent<CanvasGroup>();
+
+        if (canvas != null)
+        {
+            canvas.alpha = 0;
+        }
+
+        // Reset agar bisa spawn lagi
+        currentState = CustomerState.None;
     }
 }
