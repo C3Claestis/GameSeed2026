08af03108af433fbc200e71b7817884b7762a4e4
MJ Sidik <darkimanity@gmail.com>
2026-06-30 13:29:33 +0700

    add fonts, lalu menambahkan assets sprite dari drive dan mengubah tampilan agar sesuai dengan game yang akan di buat.

--- Assets/Code/Station/Cashier/MenuSelector.cs
+++ Assets/Code/Station/Cashier/MenuSelector.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,25 +5,27 @@ using UnityEngine.UI;
 public class MenuSelector : MonoBehaviour
 {
     [SerializeField] private float falseShakeDuration = 0.5f;
-    
+
     [Header("References")]
-    [SerializeField] private GameObject menuPrefab;
-    [SerializeField] private Transform contentHolder;
     [SerializeField] private Canvas panelCanvas;
     [SerializeField] private Canvas contentCanvas;
     [SerializeField] private Button orderUpButton;
     [SerializeField] private Button menuOpenerButton;
 
+    [Header("Menu Buttons")]
+    [SerializeField] private Button[] menuButtons;     // Isi 3 Button
+    [SerializeField] private Text[] menuTexts;     // Isi 3 TMP_Text
+
     private CashierStation _cashierStation;
     private Customer _customer;
     private MenuManager _menuManager;
-    private readonly List<Menu> _menuButtonList = new();
     private StationManager _stationManager;
+
     private bool _canSelect = true;
-    private Menu _selectedMenu;
+    private MenuData _selectedMenu;
 
     public MenuData CurrentMenu { get; private set; }
-    
+
     private void Awake()
     {
         _cashierStation = GetComponentInParent<CashierStation>();
@@ -39,8 +40,10 @@ public class MenuSelector : MonoBehaviour
         _customer = _cashierStation.Customer;
         _menuManager = MenuManager.Instance;
         _stationManager = StationManager.Instance;
+
         Unbind();
         Bind();
+
         InitializeMenus();
         SetCanvasContent(false);
     }
@@ -54,130 +57,165 @@ public class MenuSelector : MonoBehaviour
             _cashierStation.OnNewCustomerEnter -= OnNewCustomerEnter;
             _cashierStation.OnNewCustomerEnter += OnNewCustomerEnter;
         }
+
         if (_customer)
         {
             _customer.OnCustomerOrderChanged -= OnCustomerOrderChanged;
             _customer.OnCustomerOrderChanged += OnCustomerOrderChanged;
         }
-        if (orderUpButton)
-        {
-            orderUpButton.onClick.AddListener(HandleOrderUp);
-            orderUpButton.interactable = _selectedMenu && _canSelect;
-        }
-    }
-    
-    private void Unbind()
-    {
-        if (_cashierStation)
-        {
-            _cashierStation.OnNewCustomerEnter -= OnNewCustomerEnter;
-        }
-        if (_customer)
-        {
-            _customer.OnCustomerOrderChanged -= OnCustomerOrderChanged;
-        }
+
         if (orderUpButton)
         {
             orderUpButton.onClick.RemoveListener(HandleOrderUp);
+            orderUpButton.onClick.AddListener(HandleOrderUp);
+            orderUpButton.interactable = _selectedMenu != null && _canSelect;
         }
+    }
 
+    private void Unbind()
+    {
+        if (_cashierStation)
+            _cashierStation.OnNewCustomerEnter -= OnNewCustomerEnter;
+
+        if (_customer)
+            _customer.OnCustomerOrderChanged -= OnCustomerOrderChanged;
+
+        if (orderUpButton)
+            orderUpButton.onClick.RemoveListener(HandleOrderUp);
     }
 
     #endregion
-    
-    #region Canvas Manipulation
+
+    #region Canvas
 
     private void SetCanvasContent(bool value)
     {
-        if (!contentCanvas) return;
-        contentCanvas.enabled = value;
+        if (contentCanvas)
+            contentCanvas.enabled = value;
     }
 
     private void SetCanvasPanel(bool value)
     {
-        if (!panelCanvas) return;
-        panelCanvas.enabled = value;
+        if (panelCanvas)
+            panelCanvas.enabled = value;
     }
 
     #endregion
-    
+
     private void OnNewCustomerEnter()
     {
         SetCanvasContent(false);
     }
-    
+
     private void OnCustomerOrderChanged(MenuData obj)
     {
-        print($"OnCustomerOrderChanged: {obj} | {menuPrefab} | {_menuManager}");
-    }
-    
-    private void InitializeMenus()
-    {
-        if (!menuPrefab) return;
-        foreach (var data in _menuManager.MenuData)
-        {
-            var menuGo = contentHolder ? Instantiate(menuPrefab, contentHolder) : Instantiate(menuPrefab, transform);
-            if (menuGo.TryGetComponent(out Menu menu))
-            {
-                _menuButtonList.Add(menu);
-                menu.menuData = data;
-                menu.SetText();
-                menu.OnSelected += HandleMenuButtonClicked;
-            }
-        }
+        Debug.Log($"Customer Order : {obj.name}");
     }
 
-    private void HandleMenuButtonClicked(Menu target)
+    private void InitializeMenus()
     {
-        if (target && _selectedMenu != target) _selectedMenu = target;
-        else if (target && _selectedMenu == target) _selectedMenu = null;
-        
-        if (orderUpButton) orderUpButton.interactable = _selectedMenu && _canSelect;
-        
-        foreach (var menu in _menuButtonList)
+        if (_menuManager == null)
+            return;
+
+        for (int i = 0; i < menuButtons.Length; i++)
         {
-            if (menu != _selectedMenu)
+            if (i >= _menuManager.MenuData.Count)
             {
-                menu.Button.image.color = Color.white;
+                menuButtons[i].gameObject.SetActive(false);
                 continue;
             }
 
-            menu.Button.image.color = Color.yellow;
+            var data = _menuManager.MenuData[i];
+
+            menuButtons[i].gameObject.SetActive(true);
+
+            // Ganti MenuName sesuai field pada MenuData milikmu
+            menuTexts[i].text = data.menuName;
+
+            int index = i;
+
+            menuButtons[i].onClick.RemoveAllListeners();
+            menuButtons[i].onClick.AddListener(() => HandleMenuButtonClicked(index));
+
+            menuTexts[i].color = Color.black;
         }
     }
-    
+
+    private void HandleMenuButtonClicked(int index)
+    {
+        if (index >= _menuManager.MenuData.Count)
+            return;
+
+        if (_selectedMenu == _menuManager.MenuData[index])
+            _selectedMenu = null;
+        else
+            _selectedMenu = _menuManager.MenuData[index];
+
+        if (orderUpButton)
+            orderUpButton.interactable = _selectedMenu != null && _canSelect;
+
+        RefreshButtonColors();
+    }
+
+    private void RefreshButtonColors()
+    {
+        for (int i = 0; i < menuTexts.Length; i++)
+        {
+            if (i >= _menuManager.MenuData.Count)
+                continue;
+
+            menuTexts[i].color =
+                _selectedMenu == _menuManager.MenuData[i]
+                ? Color.green
+                : Color.black;
+        }
+    }
+
     private void HandleOrderUp()
     {
-        if (!_selectedMenu || !_customer || _selectedMenu.menuData == null ||
+        if (_selectedMenu == null ||
+            !_customer ||
             _customer.MenuSource == null ||
-            _selectedMenu.menuData != _customer.MenuSource)
+            _selectedMenu != _customer.MenuSource)
         {
             _canSelect = false;
-            orderUpButton.interactable = _canSelect;
+
+            orderUpButton.interactable = false;
             orderUpButton.image.color = Color.red;
-            orderUpButton.transform.DOShakePosition(falseShakeDuration, strength: 20f, vibrato: 30)
+
+            orderUpButton.transform
+                .DOShakePosition(falseShakeDuration, strength: 20f, vibrato: 30)
                 .OnComplete(() =>
                 {
                     _canSelect = true;
-                    orderUpButton.interactable = _selectedMenu && _canSelect;
+                    orderUpButton.interactable = _selectedMenu != null;
                     orderUpButton.image.color = Color.white;
                 });
+
             return;
         }
 
-        if (!CurrentMenu || CurrentMenu != _selectedMenu.menuData)
+        if (!CurrentMenu || CurrentMenu != _selectedMenu)
         {
-            CurrentMenu = Instantiate(_selectedMenu.menuData);
+            CurrentMenu = Instantiate(_selectedMenu);
         }
-        
-        if (!_stationManager) return;
+
         SetPanelMenu(false);
-        _stationManager.GoToStation(_cashierStation.StationId, _stationManager.GetStation<PrepStation>()?.StationId ?? 0);
+
+        if (_stationManager)
+        {
+            _stationManager.GoToStation(
+                _cashierStation.StationId,
+                _stationManager.GetStation<PrepStation>()?.StationId ?? 0);
+        }
     }
 
     public void SetPanelMenu(bool show)
     {
-        if (contentCanvas) contentCanvas.enabled = show;
-        if (menuOpenerButton) menuOpenerButton.interactable = !show;
+        if (contentCanvas)
+            contentCanvas.enabled = show;
+
+        if (menuOpenerButton)
+            menuOpenerButton.interactable = !show;
     }
 }
